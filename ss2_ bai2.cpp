ab55eb2111e3d1266cefcda7be7ff47aacd8f34e
DoanTheTung <doanthetung126@gmail.com>
2024-11-21 06:54:39 +0700

    upload bài 2

---
+++ ss2_ bai2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+
+int main() {
+	//Khai hai bien so nguyen
+	int a = 3;
+	int b = 5;
+	//Khai bao bien tong 
+	int sum;
+	//tinh tong hai so nguyen
+	sum = a + b;
+	//In ra ket qua
+	printf("tong cua %d và %d là: %d\n", a, b, sum);
+	
+	return 0;
+}
