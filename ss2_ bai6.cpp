f4ed7bef846dcdd5345bc45dcd01637a80e04855
DoanTheTung <doanthetung126@gmail.com>
2024-11-21 12:18:46 +0700

    upload bài 6

---
+++ ss2_ bai6.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+
+int main() {
+	//Khai bao bien pi và bá kính hình tròn 
+	float a = 3.0; 
+	const float PI = 3.14; 
+	//Khai bao bien nho ket qua
+	float chuvi , dientich; 
+	//tinh chuvi cua hinh tron 
+	chuvi = 2 * PI * a;
+	//tinh dientich cua hinh tron 
+	dientich = PI * a * a; 
+	//In ra ket qua
+	printf("chuvi  hinh tron la %d\n", chuvi);
+	printf("dientich hinh tron là %d\n", dientich);
+	
+	return 0;
+}
