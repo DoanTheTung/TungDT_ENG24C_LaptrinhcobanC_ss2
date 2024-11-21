9a24ea1be3d18b093ccb23bf7fc70c0798b53c64
DoanTheTung <doanthetung126@gmail.com>
2024-11-21 12:07:58 +0700

    upoad bài 4

---
+++ ss2_ bai4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+
+int main() {
+	//Khai bao bien canh cua hinh vuong 
+	int a = 3;
+	//Khai bao bien nho ket qua
+	int chuvi , dientich; 
+	//tinh chuvi cua hinh vuong 
+	chuvi = 4 * a;
+	//tinh dientich cua hinh vuong 
+	dientich = a * a; 
+	//In ra ket qua
+	printf("chuvi  hinh vuong la %d\n", chuvi);
+	printf("dientich hinh vuong là %d\n", dientich);
+	
+	return 0;
+}
