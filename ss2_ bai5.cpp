70be9ed74ad4e2ced0515a8259cba388d950d28d
DoanTheTung <doanthetung126@gmail.com>
2024-11-21 12:10:54 +0700

    upload bài 5

---
+++ ss2_ bai5.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+
+int main() {
+	//Khai bao bien cdai và crong cua hcn 
+	int a = 3;
+	int b = 2; 
+	//Khai bao bien nho ket qua
+	int chuvi , dientich; 
+	//tinh chuvi cua hcn 
+	chuvi = (a + b) * 2;
+	//tinh dientich cua hcn 
+	dientich = a * b; 
+	//In ra ket qua
+	printf("chuvi  hcn la %d\n", chuvi);
+	printf("dientich hcn là %d\n", dientich);
+	
+	return 0;
+}
